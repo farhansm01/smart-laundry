cdc7c0677f3234f0c1d1f9a6de849d73989d7e1c
Farhan Sadiq <farhansadiq2021@gmail.com>
2026-09-02 01:57:37 +0600

    project initialization

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+// Config Constants for Smart Laundry Application
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://localhost/dashboard/smart-laundry');
+define('SITENAME', 'Smart Laundry System');

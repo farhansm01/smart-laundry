463d1aee8b76beeb64354ebefca8d9e3adcc090a
nanditabanik47@gmail.com <nanditabanik47@gamil.com>
2026-09-04 22:55:14 +0600

    Document owner role setting

--- views/owner_dashboard.php
+++ views/owner_dashboard.php
@@ -6,8 +6,7 @@ if (!isset($_SESSION['username'])) {
     header("Location: ../login.php");
     exit();
 }
-
-$username = $_SESSION['username'];
+$role = isset($_SESSION['role']) ? $_SESSION['role'] : 'Owner'; // Set default owner role
 $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'Owner';
 ?>
 <!DOCTYPE html>

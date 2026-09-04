c589c6224a2264e81693083a20b69b852a2c012f
F4RH4M <23-52596-2@student.aiub.edu>
2026-09-04 20:28:11 +0600

    added functions to fetch orders and staff members

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -4,3 +4,14 @@
 
 require_once __DIR__ . '/../config/db.php';
   require_once __DIR__ . '/../models/OrderModel.php';
+  // Fetch all orders placed by all customers
+function fetchAllOrders() {
+    global $conn;
+  return getAllOrders($conn);
+}
+
+  // Fetch list of registered staff members
+function fetchStaffMembers() {
+   global $conn;
+    return getRegisteredStaffUsers($conn);
+}

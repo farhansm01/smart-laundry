4a25b22788efb062b3e0cad91888f7b1946e83a8
nanditabanik47@gmail.com <nanditabanik47@gamil.com>
2026-09-04 21:22:10 +0600

    Add owner dashboard sidebar structure

--- views/owner_dashboard.php
+++ views/owner_dashboard.php
@@ -18,3 +18,18 @@ $role = isset($_SESSION['role']) ? $_SESSION['role'] : 'Owner';
     <title>Owner Dashboard | Smart Laundry</title>
 </head>
 <body>
+    <div class="dashboard-wrapper">
+
+    <!-- Left Sidebar Navigation -->
+    <aside class="sidebar">
+        <div class="sidebar-header">
+            <h2>
+                <i class="fa-solid fa-soap"></i>
+                Smart Laundry
+            </h2>
+            <small>Owner Portal</small>
+        </div>
+
+        <ul class="sidebar-menu">
+        </ul>
+    </aside>

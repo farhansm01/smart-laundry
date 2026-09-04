8412477a9b15a192692b1adb4f06ed7d0844e0ce
F4RH4M <23-52596-2@student.aiub.edu>
2026-09-04 20:18:25 +0600

    started admin controller and connected to database

---
+++ controllers/AdminController.php
@@ -0,0 +1,6 @@
+<?php
+// controllers/AdminController.php
+// Handles admin operations like viewing all customer orders, accepting, rejecting, and assigning orders to registered staff
+
+require_once __DIR__ . '/../config/db.php';
+  require_once __DIR__ . '/../models/OrderModel.php';
